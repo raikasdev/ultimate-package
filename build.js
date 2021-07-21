6b8dcc15dc67536723da64aa229eba74204613fb
raikasdev <torvalds@linux.org>
2021-07-21 08:43:04 +0000

    Fix

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
 const names = require("all-the-package-names")
 
 const package = require('./package.json');
+const search = require('libnpmsearch')
 
 package.dependencies = {}
+fs = require('fs')
 
 // <3 https://github.com/Zalastax/no-one-left-behind
 const scopedPackagePattern = new RegExp('^(?:@([^/]+?)[/])?([^/]+?)$')
@@ -11,7 +13,7 @@ function urlFriendly(name) {
 }
 function validScopedName(name) {
   const nameMatch = name.match(scopedPackagePattern)
-  if (nameMatch) {
+  if (nameMatch && !name.includes('_')) {
     return urlFriendly(nameMatch[1]) && urlFriendly(nameMatch[1])
   }
 }
@@ -24,7 +26,13 @@ function sanitize(name) {
 }
 
 // back own code
-names.map(sanitize).filter(validName).forEach((name) => { package.dependencies[name.replaceAll(' ','')] = 'latest' })
+Promise.all(names.map(sanitize).filter(validName).map(async (name) => { 
+//  const s = await search(name);
+  //if (s.length === 0 || s[0].name !== name) return;
+  package.dependencies[name.replaceAll(' ','')] = 'latest'
+  return;
+}))
+.then(() => {
 
 fs = require('fs')
 fs.writeFile('package.json', JSON.stringify(package, null, 2), function (err,data) {
@@ -33,3 +41,4 @@ fs.writeFile('package.json', JSON.stringify(package, null, 2), function (err,dat
   }
   console.log(`Saved ${Object.keys(package.dependencies).length} dependencies`);
 });
+})
