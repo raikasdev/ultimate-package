96dca76b0de07ea133ec12c38e408d4b7a3424df
torvalds <torvalds@linux.org>
2021-07-21 08:31:34 +0000

    Fix

--- build.js
+++ build.js
@@ -4,7 +4,27 @@ const package = require('./package.json');
 
 package.dependencies = {}
 
-names.forEach((name) => { package.dependencies[name.replaceAll(' ','')] = 'latest' })
+// <3 https://github.com/Zalastax/no-one-left-behind
+const scopedPackagePattern = new RegExp('^(?:@([^/]+?)[/])?([^/]+?)$')
+function urlFriendly(name) {
+  return name === encodeURIComponent(name);
+}
+function validScopedName(name) {
+  const nameMatch = name.match(scopedPackagePattern)
+  if (nameMatch) {
+    return urlFriendly(nameMatch[1]) && urlFriendly(nameMatch[1])
+  }
+}
+function validName(name) {
+  return name.length > 0 && (urlFriendly(name) || validScopedName(name));
+}
+
+function sanitize(name) {
+  return name.trim(); // validName will filter out anything else that is a problem
+}
+
+// back own code
+names.map(sanitize).filter(validName).forEach((name) => { package.dependencies[name.replaceAll(' ','')] = 'latest' })
 
 fs = require('fs')
 fs.writeFile('package.json', JSON.stringify(package, null, 2), function (err,data) {
